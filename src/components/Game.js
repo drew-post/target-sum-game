a7b25f6c27771a841b2d30a2ed44adc3342aacfc
drew-post <drew_post@outlook.com>
2022-10-27 16:49:56 -0500

    First Commit

---
+++ src/components/Game.js
@@ -0,0 +1,159 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {View, Text, Button, StyleSheet} from 'react-native';
+
+import RandomNumber from './RandomNumber';
+import shuffle from 'lodash.shuffle';
+
+class Game extends React.Component {
+  static propTypes = {
+    randomNumberCount: PropTypes.number.isRequired,
+    initialSeconds: PropTypes.number.isRequired,
+    onPlayAgain: PropTypes.func.isRequired,
+  };
+
+  state = {
+    selectedIds: [],
+    remainingSeconds: this.props.initialSeconds,
+  };
+
+  gameStatus = 'PLAYING';
+
+  randomNumbers = Array.from({length: this.props.randomNumberCount}).map(
+    () => 1 + Math.floor(10 * Math.random()),
+  );
+
+  target = this.randomNumbers
+    .slice(0, this.props.randomNumberCount - 2)
+    .reduce((acc, curr) => acc + curr, 0);
+
+  shuffledRandomNumbers = shuffle(this.randomNumbers);
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState(
+        prevState => {
+          return {remainingSeconds: prevState.remainingSeconds - 1};
+        },
+        () => {
+          if (this.state.remainingSeconds === 0) {
+            clearInterval(this.intervalId);
+          }
+        },
+      );
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  isNumberSelected = numberIndex => {
+    return this.state.selectedIds.indexOf(numberIndex) >= 0;
+  };
+
+  selectNumber = numberIndex => {
+    this.setState(prevState => ({
+      selectedIds: [...prevState.selectedIds, numberIndex],
+    }));
+  };
+
+  UNSAFE_componentWillUpdate(nextProps, nextState) {
+    if (
+      nextState.selectedIds !== this.state.selectedIds ||
+      nextState.remainingSeconds === 0
+    ) {
+      this.gameStatus = this.calcGameStatus(nextState);
+      if (this.gameStatus !== 'PLAYING') {
+        clearInterval(this.intervalId);
+      }
+    }
+  }
+
+  calcGameStatus = nextState => {
+    const sumSelected = nextState.selectedIds.reduce((acc, curr) => {
+      return acc + this.shuffledRandomNumbers[curr];
+    }, 0);
+
+    if (nextState.remainingSeconds === 0) {
+      return 'LOST';
+    } else if (sumSelected < this.target) {
+      return 'PLAYING';
+    } else if (sumSelected === this.target) {
+      return 'WON';
+    } else {
+      return 'LOST';
+    }
+  };
+
+  render() {
+    const gameStatus = this.gameStatus;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.timer}>{this.state.remainingSeconds}</Text>
+        <Text style={[styles.target, styles[`STATUS_${gameStatus}`]]}>
+          {this.target}
+        </Text>
+        <View style={styles.randomContainer}>
+          {this.shuffledRandomNumbers.map((randomNumber, index) => (
+            <RandomNumber
+              key={index}
+              id={index}
+              number={randomNumber}
+              isDisabled={
+                this.isNumberSelected(index) || gameStatus !== 'PLAYING'
+              }
+              onPress={this.selectNumber}
+            />
+          ))}
+        </View>
+        {this.gameStatus !== 'PLAYING' && (
+          <Button title="Play Again" onPress={this.props.onPlayAgain} />
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FEFBEA',
+    flex: 1,
+    paddingTop: 60,
+    paddingBottom: 60,
+  },
+
+  target: {
+    fontSize: 50,
+    margin: 50,
+    textAlign: 'center',
+  },
+
+  randomContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+
+  STATUS_PLAYING: {
+    backgroundColor: '#89CFF0',
+  },
+
+  STATUS_WON: {
+    backgroundColor: '#5F8575',
+  },
+
+  STATUS_LOST: {
+    backgroundColor: '#C04000',
+  },
+
+  timer: {
+    textAlign: 'center',
+    paddingTop: 25,
+    fontSize: 20,
+  },
+});
+
+export default Game;
---
+++ src/components/RandomNumber.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+class RandomNumber extends React.Component {
+  static propTypes = {
+    number: PropTypes.number.isRequired,
+    isDisabled: PropTypes.bool.isRequired,
+    onPress: PropTypes.func.isRequired,
+    id: PropTypes.number.isRequired,
+  };
+  handlePress = () => {
+    if (!this.props.isDisabled) {
+      this.props.onPress(this.props.id);
+    }
+    return;
+  };
+  render() {
+    return (
+      <TouchableOpacity onPress={this.handlePress}>
+        <Text style={[styles.random, this.props.isDisabled && styles.disabled]}>
+          {this.props.number}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  random: {
+    backgroundColor: '#89CFF0',
+    width: 100,
+    marginHorizontal: 15,
+    marginVertical: 25,
+    fontSize: 35,
+    textAlign: 'center',
+  },
+  disabled: {
+    opacity: 0.4,
+  },
+});
+
+export default RandomNumber;
